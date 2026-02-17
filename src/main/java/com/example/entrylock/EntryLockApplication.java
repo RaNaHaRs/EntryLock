cdfb22cf3ed3f9e65d08cf701bbbc74630a9590f
Rana Harsh <hr160383@gmail.com>
2026-02-17 19:17:01 +0530

    inital project setup

---
+++ src/main/java/com/example/entrylock/EntryLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrylock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntryLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntryLockApplication.class, args);
+    }
+
+}
